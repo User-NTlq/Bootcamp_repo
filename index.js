ff359d644b8dd4e2c09477dfe7948ea0d7e89969
SpatikaPrusty <159013971+SpatikaPrusty@users.noreply.github.com>
2024-02-05 19:56:19 +0530

    Uploading files

---
+++ index.js
@@ -0,0 +1,19 @@
+const http = require("http");
+const fs = require("fs");
+const myServer = http.createServer((req, res) => {
+    const log = `${Date.now()}: ${req.url} New request received\n`;
+    fs.appendFile("log.txt", log, (err,data) => {
+        switch(req.url){
+            case '/': res.end("Home ");
+            break;
+            case '/about': res.end("I am Spatika");
+            break;
+            case '/contact-me': res.end("email: spatikaprusty@lumiq.ai");
+            break;
+            default: res.end("404 Not Found");
+        }
+        // res.end("Server says Hello!");
+    })
+});
+
+myServer.listen(8003,() => console.log("Server has started!"));
